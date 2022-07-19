fdf6a1fc3762837b85f6593494ea5d2e8fa43e97
Milo_Lynch <Milobravman20@gmail.com>
2022-07-18 22:31:55 -0400

    not sure the best way to fetch a audio file or is fetch really only for json so how to I best handle 100,000 audio files

--- spell-bee/src/App.js
+++ spell-bee/src/App.js
@@ -2,17 +2,18 @@ import './App.css';
 
 function App() {
 
-  // componentDidMount(){
-
     fetch('http://localhost:3005/search')
       .then((res) => res.json())
       .then((json) => {
         console.log(json);
       })
-
-      // this fetch may break if this is not a proxy server in the package.json the proxy is set to localhost:5000 and may not work in production.
-
-  // }
+  
+  fetch('http://localhost:3005/audio')
+    .then((res) => res.mp3())
+    .then((music) => {
+      console.log(music);
+    })
+  
 
   return (
     <div className="App">
